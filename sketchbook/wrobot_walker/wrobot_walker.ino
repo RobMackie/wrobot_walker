4e733e5c247c0b03c026c3b44962c11ac2ca11a2
unknown <Wake Robotics Member@hotglue.wakerobotics.org>
2015-12-31 11:30:50 -0500

    Committer: Rob Mackie  On branch master  Your branch is up-to-date with 'origin/master'.  Changes to be committed: 	new file:   libraries/SoftI2CMaster-master/README.md 	new file:   libraries/SoftI2CMaster-master/SoftI2CMaster.h 	new file:   libraries/SoftI2CMaster-master/examples/BMA020Soft/BMA020Soft.ino 	new file:   libraries/SoftI2CMaster-master/examples/Eeprom24AA1024Soft/Eeprom24AA1024Soft.ino 	new file:   libraries/SoftI2CMaster-master/examples/EepromFill/EepromFill.ino 	new file:   libraries/SoftI2CMaster-master/examples/EepromFill/sendfiles.pl 	new file:   libraries/SoftI2CMaster-master/examples/I2CScanSoft/I2CScanSoft.ino 	new file:   libraries/SoftI2CMaster-master/examples/MLX90614Soft/MLX90614Soft.ino 	new file:   libraries/SoftI2CMaster-master/examples/TSL2561Soft/TSL2561Soft.h 	new file:   libraries/SoftI2CMaster-master/examples/TSL2561Soft/TSL2561Soft.ino 	new file:   libraries/SoftI2CMaster-master/examples/stretchTest/stretchTest.ino 	new file:   libraries/SoftI2CMaster-master/license.txt 	modified:   wrobot_walker/wrobot_walker.ino

--- sketchbook/wrobot_walker/wrobot_walker.ino
+++ sketchbook/wrobot_walker/wrobot_walker.ino
@@ -1,9 +1,53 @@
-basvoid setup() {
+#include <Wire.h>
+
+#define PIN_ROLE_I2C_MASTER 2
+
+void setup() {
   // put your setup code here, to run once:
-  
+  if (is_master_i2c()) {
+      Wire.begin();
+  } else {
+      Wire.begin(8);                // join i2c bus with address #8
+      Wire.onReceive(receiveEvent); // register event
+      Serial.begin(9600);   
+  }
+  // setup that applies to both boards
+  pinMode(PIN_ROLE_I2C_MASTER, INPUT_PULLUP);  
 }
 
+byte x = 0;
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  // test
+  if (is_master_i2c()) {
+      digitalWrite(13, HIGH); 
+      // put your main code here, to run repeatedly:
+      Wire.beginTransmission(8); // transmit to device #8
+      Wire.write("x is ");        // sends five bytes
+      Wire.write(x);              // sends one byte
+      Wire.endTransmission();    // stop transmitting
+    
+      x++;
+      delay(500);
+  } else {
+      digitalWrite(13, LOW);
+      delay(100);    
+  }
+}
+
+
+// function that executes whenever data is received from master
+// this function is registered as an event, see setup()
+void receiveEvent(int howMany) {
+    while (1 < Wire.available()) { // loop through all but the last
+        char c = Wire.read();      // receive byte as a character
+        Serial.print(c);           // print the character
+    }
+    int x = Wire.read();        // receive byte as an integer
+    Serial.println(x);          // print the integer
+}
+
+// Returns true if the pin PIN_ROLE_I2C_MASTER is connected to 5V.
+bool is_master_i2c(void) {
+  int role = digitalRead(PIN_ROLE_I2C_MASTER == HIGH);
+  return (role);
 }
